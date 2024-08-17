91535182dfd97d372de6143f4b39bcd0673129e0
pashkinms <pashkinms86@gmail.com>
2024-08-17 11:41:30 +0700

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,7 @@
+# 3rd program
+
+no_priority = 2 * 2 + 2
+plus_priority = 2 * (2 + 2)
+print(no_priority)
+print(plus_priority)
+print(no_priority==plus_priority)
---
+++ Task4.py
@@ -0,0 +1,12 @@
+# 4th program
+
+Str = "123.456"
+
+str_float = float(Str)
+str_float *= 10
+
+print(str_float)
+
+str_int = int(str_float)
+
+print (str_int % 10)
